bd2092c66e150f4c9b3be5146d374f6aee27933e
zhuoyikang <zhuoyikang@gmail.com>
2017-05-10 23:29:30 +0800

    sync

--- src/LayaSample.js
+++ src/LayaSample.js
@@ -8,13 +8,8 @@ function Sprite_Example()
 {
     Laya.init(MapWidth, MapHeight);//设置游戏画布宽高、渲染模式。
     Laya.stage.bgColor = "#efefef";//设置画布的背景颜色。
-    Setup();
-
-    onInit();
-}
-
-function onInit()
-{
-    var p1 = new Player(512, 512);
+    var tapJoy = new TapJoy();
+    tapJoy.Setup();
+    //var p1 = new Player(100,100);
 }
 
--- src/Tapjoy.js
+++ src/Tapjoy.js
@@ -1,99 +1,48 @@
-var Sprite  = Laya.Sprite;
-var Text    = Laya.Text;
-var Event   = Laya.Event;
-var txt;
-
-function Setup()
-{
-		createInteractiveTarget();
-		createLogger();
+function TapJoy() {
 }
 
-function createInteractiveTarget()
-{
-		var rect = new Sprite();
-		rect.graphics.drawRect(0, 0, MapWidth, MapHeight, "#D2691E");
+TapJoy.prototype.Setup = function () {
+    this.createInteractiveTarget();
+    this.createLogger();
+};
 
-		rect.x = MapWidth/2;
-		rect.y = MapHeight/2;
-    rect.width = MapWidth;
-    rect.height = MapHeight;
-    rect.pivotX = MapWidth/2;
-    rect.pivotY = MapWidth/2;
-		Laya.stage.addChild(rect);
+TapJoy.prototype.createInteractiveTarget = function () {
+    var sprite = new Laya.Sprite();
+    var Event = Laya.Event;
+
+		sprite.graphics.drawRect(0, 0, MapWidth, MapHeight, "#D2691E");
+		sprite.x = MapWidth/2;
+		sprite.y = MapHeight/2;
+    sprite.width = MapWidth;
+    sprite.height = MapHeight;
+    sprite.size(MapWidth, MapHeight);
+    sprite.pivotX = MapWidth/2;
+    sprite.pivotY = MapWidth/2;
+
+    Laya.stage.addChild(sprite);
 
 		//增加鼠标事件
-		rect.on(Event.MOUSE_DOWN, this, mouseHandler);
-		rect.on(Event.MOUSE_UP, this, mouseHandler);
-		rect.on(Event.CLICK, this, mouseHandler);
-		rect.on(Event.RIGHT_MOUSE_DOWN, this, mouseHandler);
-		rect.on(Event.RIGHT_MOUSE_UP, this, mouseHandler);
-		rect.on(Event.RIGHT_CLICK, this, mouseHandler);
-		rect.on(Event.MOUSE_MOVE, this, mouseHandler);
-		rect.on(Event.MOUSE_OVER, this, mouseHandler);
-		rect.on(Event.MOUSE_OUT, this, mouseHandler);
-		rect.on(Event.DOUBLE_CLICK, this, mouseHandler);
-		rect.on(Event.MOUSE_WHEEL, this, mouseHandler);
-}
+		sprite.on(Event.MOUSE_DOWN, this, this.mouseHandler);
+		sprite.on(Event.MOUSE_UP, this, this.mouseHandler);
+		sprite.on(Event.CLICK, this, this.mouseHandler);
+		sprite.on(Event.RIGHT_MOUSE_DOWN, this, this.mouseHandler);
+		sprite.on(Event.RIGHT_MOUSE_UP, this, this.mouseHandler);
+		sprite.on(Event.RIGHT_CLICK, this, this.mouseHandler);
+		sprite.on(Event.MOUSE_MOVE, this, this.mouseHandler);
+		sprite.on(Event.MOUSE_OVER, this, this.mouseHandler);
+		sprite.on(Event.MOUSE_OUT, this, this.mouseHandler);
+		sprite.on(Event.DOUBLE_CLICK, this, this.mouseHandler);
+		sprite.on(Event.MOUSE_WHEEL, this, this.mouseHandler);
 
-/**
- * 鼠标响应事件处理
- */
-function mouseHandler(e)
-{
-		switch (e.type)
-		{
-			  case Event.MOUSE_DOWN:
-				appendText("\n————————\n左键按下");
-				break;
-			  case Event.MOUSE_UP:
-				appendText("\n左键抬起");
-				break;
-			  case Event.CLICK:
-				appendText("\n左键点击\n————————");
-				break;
-			  case Event.RIGHT_MOUSE_DOWN:
-				appendText("\n————————\n右键按下");
-				break;
-			  case Event.RIGHT_MOUSE_UP:
-				appendText("\n右键抬起");
-				break;
-			  case Event.RIGHT_CLICK:
-				appendText("\n右键单击\n————————");
-				break;
-			  case Event.MOUSE_MOVE:
-				// 如果上一个操作是移动，提示信息仅加入.字符
-				if (/鼠标移动\.*$/.test(txt.text))
-					  appendText(".");
-				else
-					  appendText("\n鼠标移动");
-				break;
-			  case Event.MOUSE_OVER:
-				appendText("\n鼠标经过目标");
-				break;
-			  case Event.MOUSE_OUT:
-				appendText("\n鼠标移出目标");
-				break;
-			  case Event.DOUBLE_CLICK:
-				appendText("\n鼠标左键双击\n————————");
-				break;
-			  case Event.MOUSE_WHEEL:
-				appendText("\n鼠标滚轮滚动");
-				break;
-		}
-}
+    this.sprite = sprite;
 
-function appendText(value)
-{
-		txt.text += value;
-		txt.scrollY = txt.maxScrollY;
-}
+};
+
 
 /**添加提示文本*/
-function createLogger()
+TapJoy.prototype.createLogger = function ()
 {
-		txt = new Text();
-
+		var txt = new Laya.Text();
 		txt.overflow = Text.SCROLL;
 		txt.text = "请把鼠标移到到矩形方块,左右键操作触发相应事件\n";
 		txt.size(Laya.stage.width, Laya.stage.height);
@@ -101,6 +50,30 @@ function createLogger()
 		txt.fontSize = 20;
 		txt.wordWrap = true;
 		txt.color = "#FFFFFF";
-
 		Laya.stage.addChild(txt);
-}
+
+    this.txt =txt;
+};
+
+
+TapJoy.prototype.mouseHandler = function (e) {
+    var Event = Laya.Event;
+    var x = e.stageX;
+    var y = e.stageY;
+
+    switch(e.type) {
+    case Event.MOUSE_DOWN:
+				this.appendText("\n————————\n左键按下"+x+" "+y);
+				break;
+		case Event.MOUSE_UP:
+				this.appendText("\n左键抬起");
+				break;
+    }
+};
+
+TapJoy.prototype.appendText = function (value)
+{
+		this.txt.text += value;
+		this.txt.scrollY = this.txt.maxScrollY;
+};
+
